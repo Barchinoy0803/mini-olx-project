9486913f67b3f97b9ab78bb6654115ddb3fe49e0
Barchinoy0803 <barchinoyyusupova33@gmail.com>
2025-03-16 16:11:48 +0500

    Vercel.json added

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,9 +13,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
+const PASSWORD = process.env.DATABASE_PASSWORD
+
 @Module({
   imports: [UserModule, AnnouncementModule, RegionModule, CategoryModule, ColorModule, OrderModule, CommentModule, FileUploadModule,
-    MongooseModule.forRoot("mongodb+srv://barchinoy08:barchinoy0802@mini-olx.wei9h.mongodb.net/?retryWrites=true&w=majority&appName=mini-olx"),
+    MongooseModule.forRoot(`mongodb+srv://barchinoy08:${PASSWORD}@mini-olx.wei9h.mongodb.net/?retryWrites=true&w=majority&appName=mini-olx`),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'),
       serveRoot: '/image'
